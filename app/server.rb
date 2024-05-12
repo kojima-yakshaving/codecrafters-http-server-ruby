cf30a61ed561a1f617491cfc218bb32f73253d18
malkoG <rijgndqw012@gmail.com>
2024-05-13 01:19:42 +0900

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -14,9 +14,22 @@ client_socket = server.accept
 status_line = client_socket.readline.chomp
 _, path, http_version, *__ = status_line.split
 
-if path != '/'
-  client_socket.puts("HTTP/1.1 404 Not Found\r\n\r\n")
-else 
+if path == '/'
   client_socket.puts("HTTP/1.1 200 OK\r\n\r\n")
+elsif path.start_with?("/echo")
+  # status line
+  client_socket.puts("HTTP/1.1 200 OK\r\n")
+
+  # header lines
+  _, _prefix, *tokens = path.split('/')
+  body = tokens.join('/')
+  client_socket.puts("Content-Type: text/plain\r\n")
+  client_socket.puts("Content-Length: #{body.size}\r\n\r\n")
+
+  # body
+  client_socket.puts(body)
+  client_socket.puts("\r\n\r\n")
+else
+  client_socket.puts("HTTP/1.1 404 Not Found\r\n\r\n")
 end
 client_socket.close
