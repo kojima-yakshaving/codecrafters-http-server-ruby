c41edbf1f545ef8eb83467881ccae10604a8845d
malkoG <rijgndqw012@gmail.com>
2024-05-13 01:47:58 +0900

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -1,48 +1,54 @@
 require "socket"
 
 # You can use print statements as follows for debugging, they'll be visible when running tests.
-print("Logs from your program will appear here!")
 
-# Uncomment this to pass the first stage
-#
 port = ENV.fetch('PORT', 4221).to_i
 
 server = TCPServer.new(port)
 
 client_socket = server.accept
 
-buf = client_socket.read
-lines = buf.lines
-status_line, header_lines = *lines
-_, path, http_version, *__ = status_line.split
+rbuf = "" 
+until client_socket.eof?
+  rbuf += client_socket.read
+end
+lines = rbuf.lines
+status_line, *header_lines = lines
+
+_, path, http_version, *__ = status_line.chomp.split
 
 if path == '/'
-  client_socket.puts("#{http_version} 200 OK\r\n\r\n")
-elsif path == '/user_agent'
+  client_socket.puts("HTTP/1.1 200 OK\r\n\r\n")
+elsif path == '/user-agent'
   user_agent_line = ""
   header_lines.each do |header_line|
     if header_line.start_with? 'User-Agent'
-      user_agent_line = header_line.chomp
+      user_agent_line = header_line.chomp.chomp
       break
     end
-  end 
+  end
 
   _key, value = user_agent_line.split(':')
-  body = v.strip
+  body = value.strip
+
+  puts body
 
   # status line
-  client_socket.puts("#{http_version} 200 OK\r\n")
+
+  wbuf = ""
+  wbuf += "HTTP/1.1 200 OK\r\n"
   
   # header lines
-  client_socket.puts("Content-Type: text/plain\r\n")
-  client_socket.puts("Content-Length: #{body.size}\r\n\r\n")
+  wbuf += "Content-Type: text/plain\r\n"
+  wbuf += "Content-Length: #{body.size}\r\n\r\n"
 
   # body
-  client_socket.puts(body)
-  client_socket.puts("\r\n\r\n")
+  wbuf += body
+
+  client_socket.write(wbuf)
 elsif path.start_with?("/echo")
   # status line
-  client_socket.puts("#{http_version} 200 OK\r\n")
+  client_socket.puts("HTTP/1.1 200 OK\r\n")
 
   # header lines
   _, _prefix, *tokens = path.split('/')
@@ -52,8 +58,7 @@ elsif path.start_with?("/echo")
 
   # body
   client_socket.puts(body)
-  client_socket.puts("\r\n\r\n")
 else
-  client_socket.puts("#{http_version} 404 Not Found\r\n\r\n")
+  client_socket.puts("HTTP/1.1 404 Not Found\r\n\r\n")
 end
 client_socket.close
