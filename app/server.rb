0b90eb4cd28ab4d7232539c478f0644d1bf66ab0
malkoG <rijgndqw012@gmail.com>
2024-05-13 01:26:05 +0900

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -11,14 +11,38 @@ server = TCPServer.new(port)
 
 client_socket = server.accept
 
-status_line = client_socket.readline.chomp
+buf = client_socket.read
+lines = buf.lines
+status_line, header_lines = *lines
 _, path, http_version, *__ = status_line.split
 
 if path == '/'
-  client_socket.puts("HTTP/1.1 200 OK\r\n\r\n")
+  client_socket.puts("#{http_version} 200 OK\r\n\r\n")
+elsif path == '/user_agent'
+  user_agent_line = ""
+  header_lines.each do |header_line|
+    if header_line.start_with? 'User-Agent'
+      user_agent_line = header_line.chomp
+      break
+    end
+  end 
+
+  _key, value = user_agent_line.split(':')
+  body = v.strip
+
+  # status line
+  client_socket.puts("#{http_version} 200 OK\r\n")
+  
+  # header lines
+  client_socket.puts("Content-Type: text/plain\r\n")
+  client_socket.puts("Content-Length: #{body.size}\r\n\r\n")
+
+  # body
+  client_socket.puts(body)
+  client_socket.puts("\r\n\r\n")
 elsif path.start_with?("/echo")
   # status line
-  client_socket.puts("HTTP/1.1 200 OK\r\n")
+  client_socket.puts("#{http_version} 200 OK\r\n")
 
   # header lines
   _, _prefix, *tokens = path.split('/')
@@ -30,6 +54,6 @@ elsif path.start_with?("/echo")
   client_socket.puts(body)
   client_socket.puts("\r\n\r\n")
 else
-  client_socket.puts("HTTP/1.1 404 Not Found\r\n\r\n")
+  client_socket.puts("#{http_version} 404 Not Found\r\n\r\n")
 end
 client_socket.close
